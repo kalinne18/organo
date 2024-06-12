13ba0d2b6cf0fea338268e5ecefc3007aa457969
kalinne18 <kalinnemairaf@gmail.com>
2024-06-12 19:55:40 -0300

    Aula 01

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX como o React trabalha com a parte visual.
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+        </header>
+    )
+}
+
+export default Banner
