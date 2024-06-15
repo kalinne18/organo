47c7068d15b33564fabffef6d00ec8e4bc98e36a
kalinne18 <kalinnemairaf@gmail.com>
2024-06-15 20:29:07 -0300

    Aula 03

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner.js';
 import Formulario from './componentes/Formulario/index.js';
 
 function App() {
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([... colaboradores, colaborador])
+  }
+
+  const [colaboradores, setColaboradores] = useState([])
   return (
     <div className="App">
-      <Banner/>
-      <Formulario />
+      <Banner />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
     </div>
   );
 }
